7f1a27eec15294cd09c54e7d92fafeb1c4ffd9b3
Daniel Millson <danielmillson923@gmail.com>
2023-12-08 00:33:08 -0500

    Finished Day 8 part 2

--- Day8/day8.go
+++ Day8/day8.go
@@ -36,6 +36,35 @@ func SplitAndTrim(str, delim string) []string {
 	return TrimSpaces(strings.Split(str, delim))
 }
 
+func AtEnd(positions []string) bool {
+	for _, pos := range positions {
+		if string(pos[2]) != "Z" {
+			return false
+		}
+	}
+	return true
+}
+
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -51,38 +80,46 @@ func main() {
 	routePattern := scanner.Text()
 	scanner.Scan()
 
+	startingPatterns := make([]string, 0)
+
 	for scanner.Scan() {
 		mapping := SplitAndTrim(scanner.Text(), "=")
-		// fmt.Println(mapping)
 
 		key := mapping[0]
 		valuesStr := strings.TrimSuffix(strings.TrimPrefix(mapping[1], "("), ")")
 		values := SplitAndTrim(valuesStr, ",")
 
-		// fmt.Println(key, values)
+		if string(key[2]) == "A" {
+			startingPatterns = append(startingPatterns, key)
+		}
+
 		directions[key] = values
 
 	}
 
-	// fmt.Println(directions)
-
-	cur := "AAA"
-	countSteps := 0
-	routePatternIdx := 0
 	routePatternLen := len(routePattern)
+	startingPatternStepsCounts := make([]int, 0)
 
-	for cur != "ZZZ" {
-		switch string(routePattern[routePatternIdx]) {
-		case "R":
-			cur = directions[cur][1]
-		case "L":
-			cur = directions[cur][0]
+	for _, pos := range startingPatterns {
+		countSteps := 0
+		routePatternIdx := 0
+		for !AtEnd([]string{pos}) {
+			dir := -1
+			switch string(routePattern[routePatternIdx]) {
+			case "R":
+				dir = 1
+			case "L":
+				dir = 0
+			}
+
+			pos = directions[pos][dir]
+
+			countSteps++
+			routePatternIdx += 1
+			routePatternIdx %= routePatternLen
 		}
-		countSteps++
-		routePatternIdx += 1
-		routePatternIdx %= routePatternLen
+		startingPatternStepsCounts = append(startingPatternStepsCounts, countSteps)
 	}
 
-	fmt.Println(countSteps)
-
+	fmt.Println(LCM(startingPatternStepsCounts[0], startingPatternStepsCounts[1], startingPatternStepsCounts...))
 }
