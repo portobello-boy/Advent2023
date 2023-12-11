e5ab085fd23674ae70906b9a8a5994c7267b4142
Daniel Millson <danielmillson923@gmail.com>
2023-12-11 01:45:37 -0500

    Finished Day 11 part 2 - cleaned it up

--- Day11/day11.go
+++ Day11/day11.go
@@ -5,48 +5,7 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"strconv"
-	"strings"
 )
-
-type Position struct {
-	X int
-	Y int
-}
-
-type QueueObject struct {
-	Pos               Position
-	DistanceFromStart int
-}
-
-// A structure to hold the necessary data for a goroutine worker
-type Job struct {
-	Id  int
-	Gal Position
-}
-
-func StringSliceToIntSlice(stringSlice []string) (intSlice []int) {
-	for _, str := range stringSlice {
-		val, _ := strconv.Atoi(str)
-		intSlice = append(intSlice, val)
-	}
-	return
-}
-
-func TrimSpaces(strSlice []string) (trimmed []string) {
-	for _, str := range strSlice {
-		if str == "" {
-			continue
-		}
-		// fmt.Printf("OG: '%v', TRIMMED: '%v'\n", str, strings.TrimSpace(str))
-		trimmed = append(trimmed, strings.TrimSpace(str))
-	}
-	return
-}
-
-func SplitAndTrim(str, delim string) []string {
-	return TrimSpaces(strings.Split(str, delim))
-}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -84,8 +43,6 @@ func main() {
 		r := grid[i]
 		if r == rowTemplate {
 			rowExpansionMap[i] = true
-			// grid = append(append(grid[:i], rowTemplate), grid[i:]...)
-			// i++
 		}
 	}
 
@@ -105,13 +62,6 @@ func main() {
 		}
 
 		colExpansionMap[i] = true
-
-		// for j := 0; j < gridLen; j++ {
-		// 	grid[j] = grid[j][:i] + "." + grid[j][i:]
-		// }
-
-		// gridWidth++
-		// i++
 	}
 
 	galaxyPositions := make([]Position, 0)
@@ -136,15 +86,7 @@ func main() {
 
 	for i, gp := range galaxyPositions {
 		for _, gp2 := range galaxyPositions[i+1:] {
-			// dist := int(math.Abs(float64(gp.X)-float64(gp2.X)) + math.Abs(float64(gp.Y)-float64(gp2.Y)))
 			dist := 0
-			// fmt.Printf("Distance from (%d, %d) to (%d, %d) is %d before expanding\n", gp.X, gp.Y, gp2.X, gp2.Y, dist)
-			// for r := gp.X; r < int(math.Abs(float64(gp.X)-float64(gp2.X))); r++ {
-			// 	if _, ok := rowExpansionMap[r]; ok {
-			// 		// fmt.Printf("row %d is expanded!\n", r)
-			// 		dist += scale
-			// 	}
-			// }
 
 			for r := math.Min(float64(gp.X), float64(gp2.X)); r < math.Max(float64(gp.X), float64(gp2.X)); r++ {
 				if _, ok := colExpansionMap[int(r)]; ok {
@@ -154,13 +96,6 @@ func main() {
 				}
 			}
 
-			// for c := gp.Y; c < int(math.Abs(float64(gp.Y)-float64(gp2.Y))); c++ {
-			// 	if _, ok := colExpansionMap[c]; ok {
-			// 		// fmt.Printf("col %d is expanded!\n", c)
-			// 		dist += scale
-			// 	}
-			// }
-
 			for c := math.Min(float64(gp.Y), float64(gp2.Y)); c < math.Max(float64(gp.Y), float64(gp2.Y)); c++ {
 				if _, ok := rowExpansionMap[int(c)]; ok {
 					dist += scale
@@ -168,7 +103,6 @@ func main() {
 					dist += 1
 				}
 			}
-			fmt.Printf("Distance from (%d, %d) to (%d, %d) is %d after expanding\n", gp.X, gp.Y, gp2.X, gp2.Y, dist)
 			sum += dist
 		}
 	}
