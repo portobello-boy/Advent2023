aaf505f15d48ff9923044795526fba92f5b304ad
Daniel Millson <danielmillson923@gmail.com>
2023-12-11 01:42:03 -0500

    Finished Day 11 part 2

--- Day11/day11.go
+++ Day11/day11.go
@@ -76,12 +76,16 @@ func main() {
 		rowTemplate += "."
 	}
 
+	rowExpansionMap := make(map[int]bool)
+	colExpansionMap := make(map[int]bool)
+
 	// Expand the galaxy rows
 	for i := 0; i < gridLen; i++ {
 		r := grid[i]
 		if r == rowTemplate {
-			grid = append(append(grid[:i], rowTemplate), grid[i:]...)
-			i++
+			rowExpansionMap[i] = true
+			// grid = append(append(grid[:i], rowTemplate), grid[i:]...)
+			// i++
 		}
 	}
 
@@ -100,12 +104,14 @@ func main() {
 			continue
 		}
 
-		for j := 0; j < gridLen; j++ {
-			grid[j] = grid[j][:i] + "." + grid[j][i:]
-		}
+		colExpansionMap[i] = true
 
-		gridWidth++
-		i++
+		// for j := 0; j < gridLen; j++ {
+		// 	grid[j] = grid[j][:i] + "." + grid[j][i:]
+		// }
+
+		// gridWidth++
+		// i++
 	}
 
 	galaxyPositions := make([]Position, 0)
@@ -126,10 +132,44 @@ func main() {
 	}
 
 	sum := 0
+	scale := 1000000
 
 	for i, gp := range galaxyPositions {
 		for _, gp2 := range galaxyPositions[i+1:] {
-			sum += int(math.Abs(float64(gp.X)-float64(gp2.X)) + math.Abs(float64(gp.Y)-float64(gp2.Y)))
+			// dist := int(math.Abs(float64(gp.X)-float64(gp2.X)) + math.Abs(float64(gp.Y)-float64(gp2.Y)))
+			dist := 0
+			// fmt.Printf("Distance from (%d, %d) to (%d, %d) is %d before expanding\n", gp.X, gp.Y, gp2.X, gp2.Y, dist)
+			// for r := gp.X; r < int(math.Abs(float64(gp.X)-float64(gp2.X))); r++ {
+			// 	if _, ok := rowExpansionMap[r]; ok {
+			// 		// fmt.Printf("row %d is expanded!\n", r)
+			// 		dist += scale
+			// 	}
+			// }
+
+			for r := math.Min(float64(gp.X), float64(gp2.X)); r < math.Max(float64(gp.X), float64(gp2.X)); r++ {
+				if _, ok := colExpansionMap[int(r)]; ok {
+					dist += scale
+				} else {
+					dist += 1
+				}
+			}
+
+			// for c := gp.Y; c < int(math.Abs(float64(gp.Y)-float64(gp2.Y))); c++ {
+			// 	if _, ok := colExpansionMap[c]; ok {
+			// 		// fmt.Printf("col %d is expanded!\n", c)
+			// 		dist += scale
+			// 	}
+			// }
+
+			for c := math.Min(float64(gp.Y), float64(gp2.Y)); c < math.Max(float64(gp.Y), float64(gp2.Y)); c++ {
+				if _, ok := rowExpansionMap[int(c)]; ok {
+					dist += scale
+				} else {
+					dist += 1
+				}
+			}
+			fmt.Printf("Distance from (%d, %d) to (%d, %d) is %d after expanding\n", gp.X, gp.Y, gp2.X, gp2.Y, dist)
+			sum += dist
 		}
 	}
 
